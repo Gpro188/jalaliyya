98188e0b59cbc4c9d2c4d8649529322947d5df95
Gpro188 <dprobahaudheen@gmail.com>
2026-04-29 11:55:45 +0530

    Fix safe area context and babel versions

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import 'react-native-reanimated';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 import { ThemeProvider } from './src/context/ThemeContext';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <AppNavigator />
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <AppNavigator />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
